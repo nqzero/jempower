e8c681a7b0f5d18e7ce9aed126cbed6a53f2174d
lytles <seth_lytle@yahoo.com>
2016-01-14 12:16:56 -0500

    comsat - integrate code from techempower and cleanup exceptions

--- comsat/src/main/java/ComsatJetty.java
+++ comsat/src/main/java/ComsatJetty.java
@@ -1,11 +1,17 @@
 
+// mvn clean package dependency:copy-dependencies -DoutputDirectory=target
+// java -cp "target/*" -javaagent:target/quasar-core-0.7.2-jdk8.jar ComsatJetty
 
+
+
+
+import co.paralleluniverse.fibers.Fiber;
 import co.paralleluniverse.fibers.SuspendExecution;
 import co.paralleluniverse.fibers.Suspendable;
 import co.paralleluniverse.fibers.servlet.FiberHttpServlet;
-import co.paralleluniverse.strands.Strand;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.nio.charset.StandardCharsets;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -14,26 +20,12 @@ import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
 
 
-// mvn clean package dependency:copy-dependencies -DoutputDirectory=target
-// java -cp "target/*" -javaagent:target/quasar-core-0.7.2-jdk8.jar ComsatJetty
-
-
 // 9096
 // duplicate for easy running
 
 public class ComsatJetty {
     private static int delay = 0;
-
-    
-    
-    public static String doSleep() throws SuspendExecution {
-        try {
-            if (delay > 0) Strand.sleep(delay);
-            return "hello world";
-        } catch (InterruptedException ex) {
-            throw new RuntimeException(ex);
-        }
-    }
+    private static final byte[] helloWorld = "hello world".getBytes(StandardCharsets.ISO_8859_1);
 
     public static void main(String[] args) throws Exception {
         if (args.length > 0) delay = Integer.valueOf(args[0]);
@@ -45,13 +37,14 @@ public class ComsatJetty {
             @Override
             @Suspendable
             protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-                try (PrintWriter out = resp.getWriter()) {
-                    try {
-                        out.println(doSleep());
-                    } catch (SuspendExecution suspendExecution) {
-                        new AssertionError(suspendExecution);
-                    }
+                try {
+                    // from techempower
+                    if (delay > 0) Fiber.sleep(delay);
+                    resp.setContentType("text/plain");
+                    resp.setHeader("Server", "comsat-servlet");
+                    resp.getOutputStream().write(helloWorld);
                 }
+                catch (InterruptedException | SuspendExecution e) {}
             }
         }), "/hello");
         server.start();
